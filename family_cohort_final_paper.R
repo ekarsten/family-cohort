0d564e5f9f8ee84111d3fc44372e2a2b9058abee
AHKL <ahkandahalf@gmail.com>
2019-04-28 16:35:01 -0500

    breakdown of entries per city

--- family_cohort_final_paper.R
+++ family_cohort_final_paper.R
@@ -22,7 +22,7 @@ library(tidyverse)
 library(haven) # for reading stata data
 library(lfe) # for fixed effect regression
 library(stargazer) # for pretty regression tables
-
+library(xtable)
 #-----------------------------------
 # Loading In the Data
 #-----------------------------------
@@ -39,6 +39,35 @@ head(df)
 #-----------------------------------
 
 #Here is where we will clean our data
+
+#Trying to figure out #s of people in a given city
+cities <- as.data.frame(table(df$PWMETRO))
+names(cities) <- c("PWMETRO", "number")
+cities <- cities[order(cities$number),]
+cities1to45 <- cities[1:45,]
+cities46to90 <- cities[46:90,]
+cities91to135 <- cities[91:135,]
+cities136to180 <- cities[136:180,]
+cities181to225 <- cities[181:225,]
+cities226to270 <- cities[226:270,]
+cities271to300 <- cities[271:300,]
+cities301to332 <- cities[301:332,]
+
+print(xtable(cities1to45, type = "latex"), file = "C:/Users/ahkan/Desktop/Family-Cohort/family-cohort/PWMetrotables/pwmetrowhole1-45.tex")
+print(xtable(cities46to90, type = "latex"), file = "C:/Users/ahkan/Desktop/Family-Cohort/family-cohort/PWMetrotables/pwmetrowhole46-90.tex")
+print(xtable(cities91to135, type = "latex"), file = "C:/Users/ahkan/Desktop/Family-Cohort/family-cohort/PWMetrotables/pwmetrowhole91-135.tex")
+print(xtable(cities136to180, type = "latex"), file = "C:/Users/ahkan/Desktop/Family-Cohort/family-cohort/PWMetrotables/pwmetrowhole136-180.tex")
+print(xtable(cities181to225, type = "latex"), file = "C:/Users/ahkan/Desktop/Family-Cohort/family-cohort/PWMetrotables/pwmetrowhole181-225.tex")
+print(xtable(cities226to270, type = "latex"), file = "C:/Users/ahkan/Desktop/Family-Cohort/family-cohort/PWMetrotables/pwmetrowhole226-270.tex")
+print(xtable(cities271to300, type = "latex"), file = "C:/Users/ahkan/Desktop/Family-Cohort/family-cohort/PWMetrotables/pwmetrowhole271-300.tex")
+print(xtable(cities301to332, type = "latex"), file = "C:/Users/ahkan/Desktop/Family-Cohort/family-cohort/PWMetrotables/pwmetrowhole301-332.tex")
+
+
+
+#organized by year (this should work to create subsets by year, but the df is still too big) 
+df1980 <- subset(df, YEAR == 1980)
+df1990 <- subset(df, YEAR == 1990)
+df2000 <- subset(df, YEAR == 2000)
 
 #-----------------------------------
 # Figures
