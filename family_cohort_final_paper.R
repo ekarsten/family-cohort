f809eee73625363d55bc461e0d006aaa9a01fa82
April3112 <yunshu@uchicago.edu>
2019-05-05 15:46:13 -0500

    figure prep

--- family_cohort_final_paper.R
+++ family_cohort_final_paper.R
@@ -15,7 +15,7 @@ while(basename(root) != "family-cohort") {
 # This line runs the script in your data.R file so that each person can have
 # their data in a different place because everyone's file structure will be 
 # a little different
-source(file.path(root, "data.R"))
+source(file.path(root, "df.Rda"))
 
 # Loading the packages we want
 library(tidyverse)
@@ -40,16 +40,72 @@ head(df)
 
 #Here is where we will clean our data
 
+# Sunday, April 28th
+NYmetro_df <- df %>%
+  filter(PWMETRO == 5600) %>%
+  filter(MARST == 1) %>%
+  filter(RACE != 7) %>%
+  filter(RACE != 8) %>%
+  filter(RACE != 9) %>%
+  filter(RACE_SP != 7) %>%
+  filter(RACE_SP != 8) %>%
+  filter(RACE_SP != 9)
+
+NYinter_df <- NYmetro_df %>%
+  filter(RACE != RACE_SP)
+
+summary(NYmetro_df)
+
+LAmetro_df <- df %>%
+  filter(PWMETRO == 4480) 
+
 #-----------------------------------
 # Figures
 #-----------------------------------
 
+# May 5th
+
+LAmetro_df <- df %>%
+  filter(PWMETRO == 4480) %>%
+  filter(MARST == 1) %>%
+  filter(RACE != 7) %>%
+  filter(RACE != 8) %>%
+  filter(RACE != 9) %>%
+  filter(RACE_SP != 7) %>%
+  filter(RACE_SP != 8) %>%
+  filter(RACE_SP != 9)
+
+LAinter_df <- LAmetro_df %>%
+  filter(RACE != RACE_SP)
+
+LA1980_df <- LAmetro_df %>%
+  filter(YEAR == 1980)
+
+LA1980_inter_df <- LA1980_df %>%
+  filter(RACE != RACE_SP)
+
+# percentage of interracial marriage in LA in 1980
+322/8755
+# result is 0.03677898
+
+LA1990_df <- LAmetro_df %>%
+  filter(YEAR == 1990)
+
+LA1990_inter_df <- LA1990_df %>%
+  filter(RACE != RACE_SP)
+
+# percentage of interracial marriage in LA in 1990
+866/17805
+# result is 0.04863802
+
+LA2000_df <- LAmetro_df %>%
+  filter(YEAR == 2000)
+
+LA2000_inter_df <- LA2000_df %>%
+  filter(RACE != RACE_SP)
+
+# percentage of interracial marriage in LA in 2000
+958/14440
+#The result is 0.06634349
+
 #Here is where we will make our figures 
-
-
-
-
-
-
-
-
